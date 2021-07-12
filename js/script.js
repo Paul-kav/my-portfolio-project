de14160c4a2b04362722ad52da3c3a8d5c33acae
Paul-kav <paulkavuma82@gmail.com>
2021-07-12 06:19:54 -0400

    Added go to top button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-const myButton = document.querySelector("#myButton");
-myButton.addEventListener("click", (e) => {
-    alert("The button was clicked");
-});
+//Get the button:
+const mybutton = document.querySelector("#myButton");
+
+// When the user scrolls down 5px from the top of the document, show the button
+ window.onscroll = function() {scrollFunction()};
+
+ function scrollFunction() {
+   if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction(){
+   document.documentElement.scrollTop = 0; 
+};
+
+
+
