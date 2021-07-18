93feeccec5a6c73dd40882d7a9c2ec36406b0034
Paul-kav <paulkavuma82@gmail.com>
2021-07-18 02:34:24 -0400

    Modified the image sizes

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 const mybutton = document.querySelector("#myButton");
 
 // When the user scrolls down 500px from the top of the document, show the button
- window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()};
 
- function scrollFunction() {
-    if (document.documentElement.scrollTop > 500) {
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 500) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -13,9 +13,12 @@ const mybutton = document.querySelector("#myButton");
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction(){
-   document.documentElement.scrollTop = 0; 
-};
+function topFunction() {
+  document.documentElement.scrollTop = 0; 
+}
+
+
+ 
 
 
 
